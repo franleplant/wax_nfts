6d5a642e79a8b36695ec236896eb8e29e4568122
Fran Guijarro <franleplant@gmail.com>
2021-04-30 17:56:27 -0300

    better types

--- src/dal/report.ts
+++ src/dal/report.ts
@@ -1,17 +1,21 @@
+import { Sale as AMSale } from "./atomicmarket"
+
+type Sale = Omit<AMSale, "assets" | "collection" | "collection_name">
+
 export interface IReportRow {
-  template: string
+  template_id: string
   name: string
   collection: string
   schema: string
+  max_suply: number
+  issued_supply: number
+  immutable_data: any
   count: number
   avg_price_wax: number
   avg_staking_price_ratio: number
   min_staking_price_ratio: number
   max_staking_price_ratio: number
   aether_hour: number
-  // TODO for now this is an array of strings but we could get
-  // the asset json alomst directly
   assets: Array<string>
-  // TODO is it string?
-  sale_ids: Array<string>
+  sale_ids: Array<Sale>
 }
