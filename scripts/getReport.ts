61b5ba9dfd792f7821d1de26b9e321ae9c95dd32
Fran Guijarro <franleplant@gmail.com>
2021-05-01 20:37:11 -0300

    report

--- scripts/getReport.ts
+++ scripts/getReport.ts
@@ -1,8 +1,11 @@
 import fs from "fs";
 import fetch from "node-fetch";
 
+const URL =
+  "https://lif-runtime.azurewebsites.net/api/report?code=ar6CDy2rR1zglx0jE95/4WsKA90ApIRwatleryR5liTFj7/CGaPVXg==";
+
 export default async function main(): Promise<void> {
-  const res = await fetch("/report");
+  const res = await fetch(URL);
   const report = await res.json();
 
   fs.writeFileSync("./src/data/report.json", JSON.stringify(report));
